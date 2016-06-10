222d1cd19d1f5b11bb4e71bb5f07ad17fab94f3e
eaharonoff <emanuel.aharonoff@gmail.com>
2016-06-09 20:04:03 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,12 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
-
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+  GENRES = []
+  # Class Constants are available to all instances of a particular class. 
+  #Whereas instance variables, i.e. title, author, etc., are individual 
+  #to each instance of a class, class constants are shared among all instances.
+  #They all have access to the same data, and if that data should change for
+  #some reason, all instances will know about that change.
   def initialize(title)
     @title = title
   end
@@ -9,5 +14,8 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-
+  def genre= (genre)
+    @genre = genre
+    GENRES << genre
+  end
 end
